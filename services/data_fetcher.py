36ee6c1558ebdb208955e0f1ef6f79b29896c754
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2025-01-04 15:44:46 +0100

    Reduce displayed items and simplify output in data_fetcher.py
    Adjusted the number of displayed items from 15 to 10 for better readability. Simplified print statements by removing redundant "Only TRADING" text. These changes enhance clarity and streamline the output.

--- services/data_fetcher.py
+++ services/data_fetcher.py
@@ -1,7 +1,7 @@
 from binance_auth import client
 
 # Number of items to display
-NUM_PRICES_TO_DISPLAY = 15
+NUM_PRICES_TO_DISPLAY = 10
 
 if __name__ == "__main__":
     # Fetch exchange information (to filter only symbols that are 'TRADING')
@@ -12,16 +12,16 @@ if __name__ == "__main__":
 
     # Fetch current market prices
     prices = client.ticker_price()
-    print("Current Prices (Only TRADING):")
+    print("Current Prices:")
     trading_prices = [price for price in prices if price['symbol'] in active_symbols]
-    for price in trading_prices[:NUM_PRICES_TO_DISPLAY]:  # Print the first NUM_PRICES_TO_DISPLAY symbols
+    for price in trading_prices[:NUM_PRICES_TO_DISPLAY]:
         print(f"Symbol: {price['symbol']}, Price: {price['price']}")
 
     # Fetch 24h market statistics
     stats = client.ticker_24hr()
-    print("\n24h Market Statistics (Only TRADING):")
+    print("\n24h Market Statistics:")
     trading_stats = [stat for stat in stats if stat['symbol'] in active_symbols]
-    for stat in trading_stats[:NUM_PRICES_TO_DISPLAY]:  # Print the first NUM_PRICES_TO_DISPLAY symbols
+    for stat in trading_stats[:NUM_PRICES_TO_DISPLAY]:
         symbol = stat["symbol"]
         price_change_percent = stat["priceChangePercent"]
         volume = stat["volume"]
