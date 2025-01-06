c9318bfe42d45941fa64f93c27b8b66b828d5059
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2025-01-06 21:08:47 +0100

    Refactor wallet data processing in portfolio manager
    Separated logic for cleaning wallet data into a dedicated `clean_wallet` function, improving readability and modularity. Enhanced wallet processing by sorting balances and calculating US

--- services/portfolio_manager.py
+++ services/portfolio_manager.py
@@ -1,42 +1,44 @@
 from services.binance_auth import client
 from utils.file_utils import save_data_to_file
 
-def fetch_wallet():
-    import logging
-    # Fetch wallet info
-    wallet_info = client.account()
 
-    # Filter out balances where both `free` and `locked` are '0.0'
+def clean_wallet(wallet_info):
+    # Remove zero balances
     wallet_info['balances'] = [
         balance for balance in wallet_info['balances']
         if float(balance['free']) > 0.0 or float(balance['locked']) > 0.0
     ]
 
+    # Fetch coin prices
     prices = {price['symbol']: float(price['price']) for price in client.ticker_price()}
-    print(f"Prices fetched.")
 
-    # Add USDT prices only for relevant balances
+    # Add USDT values and handle USDT as a special case
     for balance in wallet_info['balances']:
-        # Special case: if the asset is USDT
         if balance['asset'] == "USDT":
             balance['value_in_usdt'] = float(balance['free']) + float(balance['locked'])
         else:
-            # For other assets, calculate based on their USDT price
             symbol = f"{balance['asset']}USDT"
             if symbol in prices:
                 balance['price_in_usdt'] = prices[symbol]
                 balance['value_in_usdt'] = float(balance['free']) * balance['price_in_usdt'] + float(
-                    balance['locked']) * \
-                                           balance['price_in_usdt']
-            else:
-                # Skip balances that cannot be converted to USDT
-                continue
+                    balance['locked']) * balance['price_in_usdt']
 
-    # Sort balances by value and then by alphabetical order
+    # Sort balances by value and then alphabetically for those with no value
     wallet_info['balances'].sort(
         key=lambda balance: (-balance.get('value_in_usdt', 0), balance['asset'])
     )
 
+    return wallet_info
+
+
+def fetch_wallet():
+    # Fetch wallet info
+    wallet_info = client.account()
+
+    # Clean the wallet data
+    wallet_info = clean_wallet(wallet_info)
+
+    # Save the wallet data
     save_data_to_file(wallet_info, "wallet_data", "wallet")
 
     return wallet_info
