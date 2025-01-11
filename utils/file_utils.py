58754634adbf03e77bd17fe25a70d3294186034f
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2025-01-11 12:47:32 +0100

    Refactor file saving utility for clarity and maintainability.
    Improved code readability by adding comments and simplifying directory preparation steps. Removed redundant inline remarks and ensured consistent formatting for better maintainability. These changes enhance the usability and clarity of the file utilities.

--- utils/file_utils.py
+++ utils/file_utils.py
@@ -4,16 +4,19 @@ import os
 from datetime import datetime
 
 def save_data_to_file(data, file_path, file_name):
+
+    # Define timestamps, filename and folder name
     timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
     filename = f"{file_name}_{timestamp}.json"
     date_folder = datetime.now().strftime("%Y-%m-%d")
 
-    base_dir = os.path.dirname(os.path.dirname(__file__))  # Navigate to project root
+    # Prepare the directory
+    base_dir = os.path.dirname(os.path.dirname(__file__))
     directory = os.path.join(base_dir, "data", file_path, date_folder)
-    os.makedirs(directory, exist_ok=True)  # Ensure target directory exists
+    os.makedirs(directory, exist_ok=True)
 
     # Save the data as JSON
-    file_path = os.path.join(directory, filename)  # Prepare file path with new structure
+    file_path = os.path.join(directory, filename)
     with open(file_path, "w") as file:
         json.dump(data, file, indent=4)
 
@@ -25,7 +28,7 @@ def load_config_values(*keys):
 
     with open(config_path, "r") as file:
         try:
-            config = yaml.safe_load(file)  # Safely load the YAML as a dictionary
+            config = yaml.safe_load(file)
         except yaml.YAMLError as e:
             raise yaml.YAMLError(f"Error while parsing the config file: {str(e)}")
 
